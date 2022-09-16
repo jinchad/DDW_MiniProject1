e067174c3892cc9716f75750338a7134da05333b
Sean Phay <seanphay@gmail.com>
2022-09-16 14:24:54 +0800

    init: copied ddw repo content

---
+++ mp_sort/app/static/library.py
@@ -0,0 +1,74 @@
+from org.transcrypt.stubs.browser import *
+import random
+
+def gen_random_int(number, seed):
+	array = []
+	for i in range(number):
+		array.append(i)
+	random.seed(seed)
+	random.shuffle(array)
+	print(array)
+	return array
+
+def generate():
+	number = 10
+	seed = 200
+	array = gen_random_int(number, seed)
+	array_str = str(array)
+	array_str = array_str.strip("[]")
+	array_str = array_str.rstrip(",")
+	array_str = array_str + "."
+	# convert the items into one single string 
+	# the number should be separated by a comma
+	# and a full stop should end the string.
+
+	# This line is to placed the string into the HTML
+	# under div section with the id called "generate"	
+	document.getElementById("generate").innerHTML = array_str  
+
+def sortnumber1():
+	'''	This function is used in Exercise 1.
+		The function is called when the sort button is clicked.
+		
+		You need to do the following:
+		- get the list of numbers from the "generate" HTML id, use document.getElementById(id).innerHTML
+		- create a list of integers from the string of numbers
+		- call your sort function, either bubble sort or insertion sort
+		- create a string of the sorted numbers and store it in array_str
+	'''
+	number_l = document.getElementById("generate").innerHTML
+	console.log(number_l)
+	pass
+
+	array_str = None
+	
+	document.getElementById("sorted").innerHTML = array_str
+
+def sortnumber2():
+	'''	This function is used in Exercise 2.
+		The function is called when the sort button is clicked.
+
+		You need to do the following:
+		- Get the numbers from a string variable "value".
+		- Split the string using comma as the separator and convert them to 
+			a list of numbers
+		- call your sort function, either bubble sort or insertion sort
+		- create a string of the sorted numbers and store it in array_str
+	'''
+	# The following line get the value of the text input called "numbers"
+	value = document.getElementsByName("numbers")[0].value
+
+	# Throw alert and stop if nothing in the text input
+	if value == "":
+		window.alert("Your textbox is empty")
+		return
+
+	# Your code should start from here
+	# store the final string to the variable array_str
+	pass
+
+	array_str = None
+
+	document.getElementById("sorted").innerHTML = array_str
+
+
